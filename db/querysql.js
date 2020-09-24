006a4e7860180de58ea259ac2b87932a66a4bf43
o2cRoss <huafeier@163.com>
2020-09-24 11:37:27 +0800

    readme

--- db/querysql.js
+++ db/querysql.js
@@ -1,19 +1,19 @@
 //定义SQL查询命令
 
-// //查询整个表数据
-// let livedataSQL = {
-//   query: 'SELECT * FROM livedata'
-// };
+//查询整个表数据
+let livedataSQL = {
+  query: 'SELECT * FROM livedata'
+};
 
-// //根据ID查询
-// const queryUserID = id => {
-//   return 'SELECT * FROM livedata WHERE id=' + id
-// };
+//根据ID查询
+const queryUserID = id => {
+  return 'SELECT * FROM livedata WHERE id=' + id
+};
 
 const queryUser = user => {
   return 'SELECT id FROM users WHERE username=' + user
 }
 
 exports.queryUser = queryUser;
-// exports.livedataSQL = livedataSQL;
-// exports.queryUserID = queryUserID;
+exports.livedataSQL = livedataSQL;
+exports.queryUserID = queryUserID;
