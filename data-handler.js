98b6380469d2d534d7469df6ce246bf945011355
o2cRoss <huafeier@163.com>
2020-11-01 20:27:37 +0800

    20201101直播视频token获取

--- data-handler.js
+++ data-handler.js
@@ -8,22 +8,6 @@ const appKey = {
   appSecret:'540693b486581293cf1762fa333a61c4'
 };
 
-var accessToken = null;
-var expireTime = null;
-
-const getVedioToken = async function(){
-  await axios
-    .post('https://open.ys7.com/api/lapp/token/get',qs.stringify(appKey), {headers:{ 'Content-Type':'application/x-www-form-urlencoded'}})
-    .then((res) => {
-      console.log(res.data.data);
-      accessToken = res.data.data.accessToken;
-      expireTime = res.data.data.expireTime;
-      })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
 //解析使用 才有fields字段
 const bodyParser = require('body-parser');
 const formidableMiddleware = require('express-formidable');
@@ -73,24 +57,53 @@ router.post('/getcn', (req, res) => {
 });
 console.log("   02、设备中文接口准备完成.");
 
-//获取视频token
-router.post('/getvediotoken', (req, res) => {
-  console.log(req.fields);//提交的表格内容在fields字段
+//截取客户IP地址
+var get_client_ip = function (req) {
+  var ipStr = req.headers['x-forwarded-for'] ||
+      req.ip ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress || '';
+  var ipReg = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
+  if (ipStr.split(',').length > 0) {
+      ipStr = ipStr.split(',')[0]
+  }
+  var ip = ipReg.exec(ipStr);
+  return ip[0];
+};
 
-  getVedioToken();
-  console.log(accessToken + "," + expireTime);
-    
-  // mypool.pool.getConnection((err, connection) => {
-  //   connection.query(mypool.querySQL.GetDeviceCN(), (err, result) => {
-  //     if(err){
-  //       console.log("cuowu1");
-  //       console.log(mypool.querySQL.AllStations());
-  //     }else{
-  //       mypool.responseJSON(res, result);
-  //     }
-  //     connection.release();
-  //   });
-  // });
+var accessToken = null;
+var expireTime = null;
+
+//获取ysy的视频token和token生命周期
+const getVedioToken = async function(){
+  await axios
+    .post('https://open.ys7.com/api/lapp/token/get',qs.stringify(appKey), {headers:{ 'Content-Type':'application/x-www-form-urlencoded'}})
+    .then((res) => {
+      //console.log(res.data.data);
+      accessToken = res.data.data.accessToken;
+      expireTime = res.data.data.expireTime;
+      })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+//给客户端提供直播视频token接口
+router.post('/getvediotoken', (req, res) => {
+  //console.log(req.fields);//提交的表格内容在fields字段
+  let clientIP = get_client_ip(req);
+  let unix_stamp = new Date().getTime();
+  
+  if(accessToken == null || (unix_stamp > expireTime)){
+    getVedioToken().then(()=>{
+      console.log(`根据TOKEN生命周期,返回给客户端${clientIP}新的萤石云token`);
+      mypool.responseJSON(res,{videoToken:accessToken});
+    });
+  }else{
+    console.log(`返回给客户端${clientIP}live的萤石云token`);
+    mypool.responseJSON(res,{videoToken:accessToken});
+  }
 });
 console.log("   03、视频密钥接口准备完成.");
 
